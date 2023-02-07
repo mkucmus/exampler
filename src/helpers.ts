02951749c71a35b7d9f2399b142081032e780a09
mkucmus <maciejkucmus@gmail.com>
2023-02-07 22:09:14 +0100

    chore: gitignore

--- src/helpers.ts
+++ src/helpers.ts
@@ -19,10 +19,27 @@ export function getStackBlitzProjectConfig({
 }): Project {
   const INDEX_HTML_TEMPLATE = (title?: string) => `
 <html>
-    <h1>${title ? title : 'Hello frontends!'}</h1>
+  <head>
+  <script src="https://cdn.tailwindcss.com"></script>
+  </head>
+  <body class="p-4">
     <div>
-        <pre id="output"></pre>
-    </div>
+    <blockquote class="p-4 my-4 border-l-4 border-gray-300 bg-gray-50 dark:border-gray-500 dark:bg-gray-800">
+    <p class="text-xs font-medium leading-relaxed text-gray-900 dark:text-white">
+    import { ${functionName} } from "${packageName}";
+    <br/>
+    <br/>
+    const result = ${isAsync ? "await" : ""} ${
+    functionToRunAfterImport ?? functionName
+  }()</p>
+</blockquote>
+  <div>
+    <h2 class="text-md mb-4 mt-4">result:</h2>
+    <pre id="output" class="text-xs"></pre>
+  </div>
+        
+  </div>
+  </body>
 </html>
 `;
 
