126e8c9d7a80dfdfb917d1a86122bc71df2e2f77
mkucmus <maciejkucmus@gmail.com>
2023-02-07 09:50:51 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { computed, unref, onMounted } from 'vue';
+import { getStackBlitzProjectConfig } from './helpers';
+import HelloWorld from './components/HelloWorld.vue';
+import sdk from '@stackblitz/sdk';
+import { useRoute } from 'vue-router';
+// Import stylesheets
+import './style.css';
+
+const route = useRoute();
+
+const packageName = computed(
+  () => route.query.packageName || '@shopware-pwa/shopware-6-client'
+);
+
+const packageVersion = computed(() => route.query.packageVersion || 'latest');
+const functionName = computed(
+  () => route.query.functionName || 'getSessionContext'
+);
+
+const isAsync = computed(() => !!route.query.async);
+
+const openExample = () => {
+  sdk.openProject(
+    getStackBlitzProjectConfig({
+      packageName: packageName.value,
+      packageVersion: packageVersion.value,
+      functionName: functionName.value,
+      isAsync: isAsync.value,
+    }),
+    { newWindow: true, clickToLoad: true }
+  );
+};
+
+onMounted(() => openExample());
+</script>
+
+<template>
+  <div>Your example configuration:</div>
+  <div class="settings">
+    <pre>{{
+      {
+        packageName,
+        packageVersion,
+        functionName,
+      }
+    }}</pre>
+  </div>
+  <!-- <div><button @click="openExample">Click to load</button></div> -->
+</template>
+
+<style>
+pre {
+  text-align: left;
+}
+
+div.settings {
+  text-align: left;
+}
+</style>
---
+++ src/helpers.ts
@@ -0,0 +1,76 @@
+import { Project } from '@stackblitz/sdk';
+
+export function getStackBlitzProjectConfig({
+  packageName,
+  packageVersion,
+  functionName,
+  isAsync,
+  multiImport,
+  functionToRunAfterImport,
+  paramsConfig,
+}: {
+  packageName: string;
+  packageVersion?: string;
+  functionName: string;
+  isAsync: boolean;
+  multiImport?: string;
+  functionToRunAfterImport?: string;
+  paramsConfig: any;
+}): Project {
+  const INDEX_HTML_TEMPLATE = (title?: string) => `
+<html>
+    <h1>${title ? title : 'Hello frontends!'}</h1>
+    <div>
+        <pre id="output"></pre>
+    </div>
+</html>
+`;
+
+  const destructureImports = () => multiImport;
+  const getFunctionToRun = () => {
+    if (!multiImport) {
+      return functionName;
+    }
+
+    if (
+      functionToRunAfterImport &&
+      multiImport.includes(functionToRunAfterImport)
+    ) {
+      return functionToRunAfterImport;
+    }
+
+    return multiImport.split(',').shift();
+  };
+
+  const INDEX_TS_TEMPLATE = () => `
+import { ${functionName} } from "${packageName}";
+
+async function runTest() {
+  const outputContainer = document.getElementById("output");
+  try {
+    const ${!multiImport ? 'result' : destructureImports()} = ${
+    isAsync || multiImport ? 'await ' : ''
+  }${getFunctionToRun()}(${paramsConfig ? JSON.stringify(paramsConfig) : ''});
+  
+    outputContainer.innerText = JSON.stringify(result, null, 2);
+  } catch (error) {
+    console.error('An error occured while running example: ', error);
+    outputContainer.innerText = "An error occured. Check the debug console to see the details.";
+  }
+}
+
+runTest();
+`;
+  return {
+    title: `${packageName} > ${functionName} example`,
+    description: `Example of using ${packageName} > ${functionName} function`,
+    template: 'typescript',
+    files: {
+      'index.html': INDEX_HTML_TEMPLATE(),
+      'index.ts': INDEX_TS_TEMPLATE(),
+    },
+    dependencies: {
+      [packageName]: packageVersion || 'latest',
+    },
+  };
+}
